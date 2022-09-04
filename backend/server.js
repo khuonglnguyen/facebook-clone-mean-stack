846b4cafe8255357316c8e23eacfab9db63d8b84
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-09-04 19:27:22 +0700

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+// let allowed = ["http://localhost:3000", "some orther link"];
+// function options(req, res) {
+//   let tmp;
+//   let origin = req.header("Origin");
+//   if (allowed.indexOf(origin) > -1) {
+//     tmp = {
+//       origin: true,
+//       optionSuccessStatus: 200,
+//     };
+//   } else {
+//     tmp = {
+//       origin: "nooooo",
+//     };
+//   }
+//   res(null, tmp);
+// }
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("welcome from home");
