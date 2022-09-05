86a3bb78cac94543bd80a8edcb52b0a7d56bca11
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-09-05 19:11:31 +0700

    setup routes

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-// let allowed = ["http://localhost:3000", "some orther link"];
-// function options(req, res) {
-//   let tmp;
-//   let origin = req.header("Origin");
-//   if (allowed.indexOf(origin) > -1) {
-//     tmp = {
-//       origin: true,
-//       optionSuccessStatus: 200,
-//     };
-//   } else {
-//     tmp = {
-//       origin: "nooooo",
-//     };
-//   }
-//   res(null, tmp);
-// }
-
+const { readdirSync } = require("fs");
 const app = express();
 app.use(cors());
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.get("/", (req, res) => {
-  res.send("welcome from home");
-});
-app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
-});
 app.listen(8000, () => {
   console.log("server is lestining...");
 });
